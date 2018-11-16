8dca0eabd8bd1394e7872fc367ccd649db00e469
Fitzfoufou <francois.fitzpatrick@gmail.com>
2018-11-16 17:05:50 +0100

    Fetch of all tattoos and artists user page

--- src/Components/FavTattoosProfile.js
+++ src/Components/FavTattoosProfile.js
@@ -1,60 +1,45 @@
 import React from 'react';
-import { CardImg } from 'reactstrap';
+import {CardImg} from 'reactstrap';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import '../Stylesheets/FavTattoosProfile.css';
 
-
-
 export default class FavTattoosProfile extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       tattoosList: []
     }
   }
 
-  // ComponentDidMount() {
-  //   var ctx= this;
-  //   fetch("http://localhost:3000/user")
-  //         .then(function(response) {
-  //             return response.json();
-  //           })
-  //         .then(function(data) {
-  //           tatoosListCopy =[...tattoosList];
-  //           tatoosListCopy.push(data.userFavoriteTattoo);
-  //           ctx.setState({
-  //             tattoosList : tatoosListCopy
-  //           })
-  //             })
-  //         .catch(function(error) {
-  //             console.log('Request failed', error)
-  //           });
-  // }
+  componentDidMount() {
+    var ctx = this;
+    fetch("http://localhost:3000/user?user_id="+"5beee9149dbdeb5bac33360e")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var tattoosListCopy = [...ctx.state.tattoosList];
+      data.result.userFavoriteTattoo.map(function(map){
+        return tattoosListCopy.push(map)
+      })
+      ctx.setState({ tattoosList: tattoosListCopy});
+    })
+    .catch(function(error) {
+      console.log('Request failed', error);
+    });
+  }
 
   render() {
-  console.log("tattoosList", this.state.tattoosList);
-  var tattoosList= [];
-    for(var i =1; i< 8; i ++){
-      tattoosList.push({src:'../tatouagesBichon/photoBichon'+ i +'.jpg', artistName:"Bichon"})
-    };
-    for(var j =1; j< 8; j ++){
-      tattoosList.push({src:'../tatouagesPrincess/photoMadness'+ i +'.jpg', artistName:"Princess Madness"})
-    };
-
-
-        var tattoosDisplayedCards = tattoosList.map(function(tattoo, i){
-          return <TattooCard
-           key={i}
-           artistName={tattoo.artistName}
-           tattooImage={tattoo.src}/>
-       })
+    var tattoosDisplayedCards = this.state.tattoosList.map(function(tattoo, i) {
+      return <TattooCard key={i} artistName="Bichon" tattooImage={tattoo.tattooPhotoLink}/>
+    });
 
     return (
       <div className="containerTattoos col-12">
-          <div className="row">
-            {tattoosDisplayedCards}
-          </div>
+        <div className="row">
+          {tattoosDisplayedCards}
+        </div>
       </div>
     );
   }
@@ -62,39 +47,34 @@ export default class FavTattoosProfile extends React.Component {
 
 class TattooCard extends React.Component {
   constructor(props) {
-   super(props);
-   this.state = {
-     isMouseOver: false
-   }
- }
+    super(props);
+    this.state = {
+      isMouseOver: false
+    }
+  }
 
+  handleMouseOver = () => {
+    this.setState({isMouseOver: true});
+  }
 
- handleMouseOver = () => {
-   this.setState({isMouseOver: true});
- }
-
- handleMouseOut = () => {
-   this.setState({isMouseOver: false});
- }
-
-
+  handleMouseOut = () => {
+    this.setState({isMouseOver: false});
+  }
 
   render() {
     console.log("this.state.isMouseOver", this.state.isMouseOver);
 
-    return(
-      <div className="containerCard col-6" onMouseOver ={this.handleMouseOver} onMouseOut={this.handleMouseOut}>
-
-           <div id="imgContainer">
-             <CardImg id ="TattooImg" src={this.props.tattooImage} alt="Card image cap"/>
-               {this.state.isMouseOver
-               ? <div className="infoArtistUnderImg">{this.props.artistName}</div>
-               :<div></div>
-             }
-           </div>
-
+    return (
+      <div className="containerCard col-6" onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}>
+        <div id="imgContainer">
+          <CardImg id="TattooImg" src={this.props.tattooImage} alt="Card image cap"/>
+          {
+            this.state.isMouseOver
+              ? <div className="infoArtistUnderImg">{this.props.artistName}</div>
+              : <div></div>
+          }
+        </div>
       </div>
-
     );
   }
 }
--- src/Screens/UserPage.js
+++ src/Screens/UserPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import NavBar from '../Components/NavBar.js'
 
 import ProjectForm from '../Components/ProjectForm.js'
@@ -7,38 +7,31 @@ import FavArtistsProfile from '../Components/FavArtistsProfile.js'
 
 import '../Stylesheets/UserPage.css';
 
+class UserPage extends Component {
+  render() {
+    return (<div className="userPageContainer">
+      <NavBar/>
+      <div className="mainContainer col-12">
+        <div className="topContainer col-12">
+          <div className="row-topFormTattoos col-12 col-sm-8">
+            Votre projet de tatouage
+            <ProjectForm/>
+          </div>
 
+          <div className="row-topImgTattoos col-12 col-sm-4">
+            Vos tatouages favoris
+            <FavTattoosProfile/>
+          </div>
+        </div>
 
-class UserPage extends Component{
-  render(){
-    return(
-      <div className="userPageContainer">
-        <NavBar />
+        <div className="lowContainer col-12">
+          Vos tatoueurs favoris
+          <FavArtistsProfile/>
+        </div>
 
-            <div className="mainContainer col-12">
-                      <div className="topContainer col-12">
-                              <div className="row-topFormTattoos col-12 col-sm-8">
-                                Votre projet de tatouage
-                                  <ProjectForm />
-                              </div>
-
-                              <div className="row-topImgTattoos col-12 col-sm-4">
-                                Vos tatouages favoris
-                                  <FavTattoosProfile />
-                              </div>
-                     </div>
-
-                  <div className="lowContainer col-12">
-                      Vos tatoueurs favoris
-                      <FavArtistsProfile />
-                  </div>
-
-            </div>
       </div>
-    )
+    </div>)
   }
 }
-
-
 
 export default UserPage;
