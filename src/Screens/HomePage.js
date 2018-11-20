7e6757c1743efbead41edf02be1b365a9c630a9a
Madian75 <madianh33@gmail.com>
2018-11-20 16:56:25 +0100

    versio

--- src/Screens/HomePage.js
+++ src/Screens/HomePage.js
@@ -13,56 +13,8 @@ class HomePage extends Component {
     return (
 
     <div className="container-homePageContainer">
-      
-
-<<<<<<< HEAD
-=======
-      <div className="homePageContent">
-        <div className="homePageContentBackgroundColor">
-          <div className="findMyTattooIntro">BIENVENUE À CELLES ET CEUX QUI PENSENT QU'ON PEUT TOUT REGRETTER
-            <br/>
-            <strong>SAUF SON TATOUAGE !</strong>
-
-          </div>
-          <div className="findMyTattooPromise">
-            <div className="findmytattooLogoPack">
-              <img src={logo}/>
-              <h1>Find My Tattoo</h1>
-
-            </div>
-            <h2>Rassemble
-              <strong> VOS TATOUEURS PREFERES </strong>
-               et leurs travaux de
-              <strong> TATOUAGES </strong>
-              pour que soyez certains de
-              <strong> TOUJOURS FAIRE LE BON CHOIX
-              </strong>
-            </h2>
-          </div>
-
-          <div className="buttonsSignInUp">
-            <SignUpForm />
-            <SignInForm />
-
-          </div>
 
 
-          <div className="homePageFunctionalities">
-            <div className="homePageSingleFunctionality col-xs-12">
-              <p className="textSingleFunctionality">INSPIREZ-VOUS DES PLUS BEAUX TATOUAGES RÉALISÉS</p>
-            </div>
-            <div className="homePageSingleFunctionality col-xs-12 ">
-              <p className="textSingleFunctionality">TROUVEZ LE TATOUEUR IDEAL POUR VOTRE PROJET</p>
-            </div>
-              <div className="homePageSingleFunctionality col-xs-12">
-                <p className="textSingleFunctionality">PRENEZ LE TEMPS DE CONSTRUIRE VOTRE TATOUAGE</p>
-              </div>
-          </div>
-        </div>
-        <GalleryPage />
-
-      </div>
->>>>>>> 8af4b8694e8de96d55ee047c1246a1e532aceec0
 
     </div>
   )}
